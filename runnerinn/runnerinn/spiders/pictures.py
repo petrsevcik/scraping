5ea416c81edce1622c2a7f4756aa8dbb80416276
petr.sevcik <petr.sevcik@kiwi.com>
2022-10-23 18:48:25 +0200

    finetuning picture scraper

--- runnerinn/runnerinn/spiders/pictures.py
+++ runnerinn/runnerinn/spiders/pictures.py
@@ -18,12 +18,12 @@ class PicturesSpider(scrapy.Spider):
     def start_requests(self):
 
         for link in self.start_urls:
-            for i in range(1,3):
+            for i in range(1,4):
                 if i == 1:
                     yield scrapy.Request(url=link, callback=self.parse)
                 else:
                     decomposed_link = link.split('/')
-                    # add to fifth element _2
+                    # add to fifth element _2/_3
                     decomposed_link[5] = decomposed_link[5] + f'_{i}'
                     new_link = '/'.join(decomposed_link)
                     yield scrapy.Request(url=new_link, callback=self.parse)
@@ -32,11 +32,11 @@ class PicturesSpider(scrapy.Spider):
 
     def parse(self, response):
         item = ImageItem()
-        item['image_name'] = response.url.split('/')[-1]
+        item['image_name'] = response.url.split('/')[-1].replace(".jpg", "") + response.url.split('/')[5] + ".jpg"
         item['image_urls'] = [response.url]
         yield item
 
-    def load_links_from_json(self, file="test_shoes.json"):
+    def load_links_from_json(self, file="road_shoes.json"):
         links = []
         with open(file, 'r') as f:
             data = json.load(f)
